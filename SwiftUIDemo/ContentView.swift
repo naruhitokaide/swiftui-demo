8a9541d746bda6767fae5d132372832162f0bc8f
KodaKDominus <kodakatsuro@gmail.com>
2020-08-06 22:16:09 +0900

    Init Commit

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Xrs on 8/6/20.
+//  Copyright © 2020 Amartis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
