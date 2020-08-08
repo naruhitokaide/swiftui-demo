fcb16a7d305ad7ce4fb383a517f0ec22fe038585
KodaKDominus <kodakatsuro@gmail.com>
2020-08-08 19:23:40 +0900

    Use List

--- SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        List {
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+        }
     }
 }
 
