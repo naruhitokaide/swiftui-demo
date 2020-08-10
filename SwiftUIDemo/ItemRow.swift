cc170b56d261ceb8bd09e27130726de8fdf8325b
KodaKDominus <kodakatsuro@gmail.com>
2020-08-11 07:41:17 +0900

    Update ItemRow : Make it stylish

--- SwiftUIDemo/ItemRow.swift
+++ SwiftUIDemo/ItemRow.swift
@@ -9,13 +9,27 @@
 import SwiftUI
 
 struct ItemRow: View {
+    static let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
     var item: MenuItem
     var body: some View {
         HStack {
             Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray, lineWidth: 2))
             VStack(alignment: .leading) {
                 Text(item.name)
+                    .font(.headline)
                 Text(String("$\(item.price)"))
+            }
+            Spacer()
+            ForEach(item.restrictions, id: \.self) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(Self.colors[restriction, default: .black])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
             }
         }
     }
